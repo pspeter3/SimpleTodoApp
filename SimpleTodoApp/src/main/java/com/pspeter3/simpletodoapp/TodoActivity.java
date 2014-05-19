aca04feb7655d1d193ee72274ffc41579a0a47e7
Phips Peter <pspeter333@gmail.com>
2014-05-19 14:53:36 -0700

    Set up the basic layout
    Nothing runs yet but it still looks super cool on my phone!

--- SimpleTodoApp/src/main/java/com/pspeter3/simpletodoapp/TodoActivity.java
+++ SimpleTodoApp/src/main/java/com/pspeter3/simpletodoapp/TodoActivity.java
@@ -4,14 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class TodoActivity extends Activity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
     }
 
 
